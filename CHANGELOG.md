214ac59a9eae6467b2baaf2d36b9245021b9a383
at-lab <dzherenashvili@yandex.ru>
2017-05-24 09:39:06 +0300

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `laravel-unitpay` will be documented in this file
+
+## 1.0.0 - 2017-02-21
+
+- initial release
---
+++ tests/Dummy/AnotherNotification.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PaataD\UnitPay\Test\Dummy;
+
+use PaataD\UnitPay\UnitPayNotification;
+
+class AnotherNotification extends UnitPayNotification
+{
+}
